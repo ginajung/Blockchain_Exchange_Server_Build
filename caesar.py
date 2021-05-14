87f82b1529549918a8d35ceee41da9835e066268
biodsjung <biodsjung@gmail.com>
2021-05-14 15:43:25 +0900

    first commit

---
+++ caesar.py
@@ -0,0 +1,26 @@
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encrypt(key,plaintext):
+    ciphertext=""
+    pt = list(plaintext)
+    for i in range (len(plaintext)):
+        for j in range (len(LETTERS)):
+            if plaintext[i]==LETTERS[j]:
+                if j+key < 26:
+                    pt[i] = LETTERS[j+key]  # need to handle end letter 
+                else:
+                    pt[i] = LETTERS[j+key-26]
+    return ciphertext.join(pt)
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    ct = list(ciphertext)
+    for i in range (len(ciphertext)):
+        for j in range (len(LETTERS)):
+            if ciphertext[i]==LETTERS[j]:
+                if j+key > 0:
+                    ct[i] = LETTERS[j-key]  # need to handle end letter 
+                else:
+                    ct[i] = LETTERS[j-key+26]
+    return plaintext.join(ct)
+
