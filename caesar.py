96b4580b71511c8bba3032d9ed7d29599ce7e645
biodsjung <biodsjung@gmail.com>
2021-05-17 11:25:04 +0900

    caesar mod updated

--- caesar.py
+++ caesar.py
@@ -7,10 +7,10 @@ def encrypt(key,plaintext):
     for i in range (len(plaintext)):
         for j in range (len(LETTERS)):
             if plaintext[i]==LETTERS[j]:
-                if j+key < 26:
-                    pt[i] = LETTERS[j+key]  # need to handle end letter 
+                keymod = key%26
+                if j+keymod < 26:
+                    pt[i] = LETTERS[j+keymod]  # need to handle end letter 
                 else:
-                    keymod = key%26
                     pt[i] = LETTERS[j+keymod-26]
     return ciphertext.join(pt)
 
@@ -20,10 +20,10 @@ def decrypt(key,ciphertext):
     for i in range (len(ciphertext)):
         for j in range (len(LETTERS)):
             if ciphertext[i]==LETTERS[j]:
-                if j+key > 0:
-                    ct[i] = LETTERS[j-key]  # need to handle end letter 
+                keymod = key%26
+                if j+keymod > 0:
+                    ct[i] = LETTERS[j-keymod]  # need to handle end letter 
                 else:
-                    keymod = key%26
                     ct[i] = LETTERS[j-keymod+26]
     return plaintext.join(ct)
 
