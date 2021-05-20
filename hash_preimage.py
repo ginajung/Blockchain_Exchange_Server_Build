8cfe308235d1cb5c6490deae679ee605e70ce7fe
biodsjung <biodsjung@gmail.com>
2021-05-20 21:10:53 +0900

    hash_preimage v1

--- hash_preimage.py
+++ hash_preimage.py
@@ -5,7 +5,21 @@ def hash_preimage(target_string):
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    nonce = b'\x00'
+    
+    
+    while(True):
+        size = 5
+        nonce = os.urandom(size)
+    
+        hex_nonce= hashlib.sha256( nonce ).hexdigest()
+        bin_nonce = bin(int(hex_nonce,base=16))[2:]
+    
+    
+        if target_string[:] == bin_nonce[-len(target_string):] and len(target_string) != len(bin_nonce):
+            return(nonce)
+        else:
+            continue
+    
 
-    return( nonce )
+
 
