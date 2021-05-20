79fc39c1fdd427197ff3781fb287bbc43f83f066
biodsjung <biodsjung@gmail.com>
2021-05-20 17:43:15 +0900

    hash_collision v1

--- hash_collision.py
+++ hash_collision.py
@@ -14,18 +14,20 @@ def hash_collision(k):
     #Collision finding code goes here
     
     # generate random bytes / compare SHA256(x)
-    while(True):
-        x = os.urandom(2) # 256 bits
-        SHA256.update(x)
-        hex_x= SHA256.hexdigest()
-        x_bin = bin(int(hex_x,base=16))[:]
-        
     
-        y = os.urandom(2)
+    x = os.urandom(1) # 256 bits
+    SHA256.update(x)
+    hex_x= SHA256.hexdigest()
+    #x_bin = bin(int(hex_x,base=16))[:]
+    x_bin = list(hex_x)[:]
+    
+    while(True):
+        y = os.urandom(1)
         SHA256.update(y)
         hex_y= SHA256.hexdigest()
-        y_bin = bin(int(hex_y,base=16))[:]
-    
-        if x_bin[-k:]==y_bin[-k:] and x !=y:
+        #y_bin = bin(int(hex_y,base=16))[:]
+        y_bin = list(hex_x)[:]
+        
+        if x_bin[-k:] == y_bin[-k:] and x !=y:
             return(x,y)
     
