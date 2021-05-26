ef2f4136f75c59e72f8b209eac53ced0dd12ba8f
biodsjung <biodsjung@gmail.com>
2021-05-27 07:49:42 +0900

    elgamal v1

--- elgamal.py
+++ elgamal.py
@@ -1,19 +1,40 @@
-import random
+from random import randint
 
 from params import p
 from params import g
 
+# p-1 = 2q
+# g is chosen to have order q , g^q = 1 mod p
+
+def ordermod(g,p):
+    x = pow(g,-1,p)
+    assert (x*g)%p ==1
+    return q
+
+   
 def keygen():
-    sk = 0
-    pk = 0
+    # if q is the order of g
+    q = ordermod(g,p)
+    a = randint(1,q)
+    
+    # if q is unknown, (1,p)
+    h = pow(g,a,p)
+    sk = a
+    pk = h
+    
     return pk,sk
 
 def encrypt(pk,m):
-    c1 = 0
-    c2 = 0
+    r = randint(1,q)
+    c1 = pow(g,r,p)   
+  # c2 = h^r *m mod p
+    mes= (pk**r)*m
+    c2 = mes % p
     return [c1,c2]
 
 def decrypt(sk,c):
-    m = 0
+    
+    c1= c[0]**sk
+    c2 =c[1]
+    m = c2/c1 % p
     return m
-
