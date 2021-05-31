53870cb2940851cafebb5b88bbae0e1ca3097c6f
biodsjung <biodsjung@gmail.com>
2021-05-31 10:54:49 +0900

    sign v1

--- sign.py
+++ sign.py
@@ -2,21 +2,34 @@ from fastecdsa.curve import secp256k1
 from fastecdsa.keys import export_key, gen_keypair
 
 from fastecdsa import curve, ecdsa, keys, point
-from hashlib import sha256
+from hashlib import sha265
+import random
+
 
 def sign(m):
-	#generate public key
+	
 	#Your code here
-	public_key = None
-
+    G = secp256k1.G
+    n = secp256k1.n
+    
+ 
+    d = random.SystemRandom().randint(1,n-1)
+    
+    #generate public key
+	public_key = d*G
+    
+    x1=public_key.x
+    y1=public_key.y
+    
 	#generate signature
 	#Your code here
-	r = 0
-	s = 0
+	r = pow(x1,1,n)
+    if (r==0):
+        
+    z = sha256( m )
+	s = pow(d,-1,n)* pow(z+rd,1,n)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
 	return( public_key, [r,s] )
-
-
