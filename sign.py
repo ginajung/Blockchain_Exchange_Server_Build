ac87c54a522c9d738a2c9ea7b3402ed19f2bf73b
biodsjung <biodsjung@gmail.com>
2021-05-31 11:27:19 +0900

    sign v1

--- sign.py
+++ sign.py
@@ -1,41 +1,41 @@
 from fastecdsa.curve import secp256k1
 from fastecdsa.keys import export_key, gen_keypair
-from fastecdsa import curve, ecdsa, keys, point
-import hashlib 
 
+from fastecdsa import curve, ecdsa, keys, point
+from hashlib import sha256
 import random
 
 
 def sign(m):
-    G = secp256k1.G
-    n = secp256k1.q
+	
     d = random.SystemRandom().randint(1,n-1)
-  
-  #generate public key
-    public_key = keys.get_public_key(d,G)
-  
-    x1=public_key.x
-    y1=public_key.y
+    
+   
+
+	#Your code here
+    #G = secp256k1.G
+    n = secp256k1.n
+    
+ 
+    
+    
+    #generate public key
+	public_key = keys.get_public_key(d,G)
+    
+#     x1=public_key.x
+#     y1=public_key.y
     
 	#generate signature
-    while(True):
-        r = pow(x1,1,n)
-        if(r==0):
-            d = random.SystemRandom().randint(1,n-1)
-        else: 
-            continue
-      
-        z = hashlibsha256( m )
-        s = pow(d,-1,n)* pow(z+rd,1,n)
-        if(s==0): 
-            d = random.SystemRandom().randint(1,n-1)
-        else:
-            continue
+	#Your code here
+# 	r = pow(x1,1,n)
+#     if (r==0):
+        
+#     z = sha256( m )
+# 	s = pow(d,-1,n)* pow(z+rd,1,n)
     
-    assert isinstance( public_key, point.Point )
-    assert isinstance( r, int )
-    assert isinstance( s, int )
+    r,s = fastecdsa.ecdsa.sign(m, d, secp256k1, sha256, prehashed: bool = False)
 
-    return( public_key, [r,s] )
-
-
+	assert isinstance( public_key, point.Point )
+	assert isinstance( r, int )
+	assert isinstance( s, int )
+	return( public_key, [r,s] )
