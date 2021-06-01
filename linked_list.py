854f06bdac74645fee94127dc0dcc9b6d622b797
biodsjung <biodsjung@gmail.com>
2021-06-01 11:19:24 +0900

    linked_list v1

---
+++ linked_list.py
@@ -0,0 +1,43 @@
+import hashlib
+class Block:
+    def __init__(self, index, timestamp, content, previous_hash):
+      self.index = index
+      self.timestamp = timestamp
+      self.content = content
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+   
+    def calc_hash(self):
+      sha = hashlib.sha256()
+      sha.update(str(self.index).encode('utf-8') + 
+                 str(self.timestamp).encode('utf-8') + 
+                 str(self.content).encode('utf-8') + 
+                 str(self.previous_hash).encode('utf-8'))
+      return sha.hexdigest()
+      
+M4BlockChain = []
+
+from datetime import datetime
+def create_genesis_block():
+    return Block(0, datetime.now(), "Genesis Block", "0")
+    
+M4BlockChain.append(create_genesis_block())
+
+
+# write a function `next_block` to generate a block
+def next_block(last_block):
+    
+    # generate block
+    next_node = Block(last_block.index+1, datetime.now(), ' content', last_block.hash)
+    return next_node
+    pass
+    
+# append 5 blocks to the blockchain
+def app_five(block_list):
+    i = 0
+    for i in range (0,5):
+        next_node = next_block(last_block)
+        M4BlockChain.append(next_node)
+        last_block = next_node
+        i += 1
+    pass
