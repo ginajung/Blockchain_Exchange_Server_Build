907b777b1715143d87e9d1106df19e7253ab7060
biodsjung <biodsjung@gmail.com>
2021-06-03 10:24:07 +0900

    linked_list v2

--- linked_list.py
+++ linked_list.py
@@ -37,7 +37,7 @@ def next_block(last_block):
     
 # append 5 blocks to the blockchain
 def app_five(block_list):
-    last_block = M4BlockChain[len(M4BlockChain)+1]
+    last_block = M4BlockChain[len(M4BlockChain)-1]
     i = 0
     for i in range (0,5):
         next_node = next_block(last_block)
