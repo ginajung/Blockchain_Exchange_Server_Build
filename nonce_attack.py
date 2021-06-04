63fa3c1369764055adee4f133a2678b011e71605
biodsjung <biodsjung@gmail.com>
2021-06-04 19:31:42 +0900

    nonce-attack v2

--- nonce_attack.py
+++ nonce_attack.py
@@ -16,27 +16,28 @@ def recoverkey( sig1, sig2, m1, m2, pk ):
         print( "Signatures were generated with different nonces" )
     
     # when r1 == r2 , recover k 
-    else:
+    
     # order of g
-        g = secp256k1.G
-        n = secp256k1.q
-        r1 = sig1[0]
-        r2 = sig2[0]
-        s1 = sig1[1]
-        s2 = sig2[1]
-        z1 = sha256(m1)
-        z2 = sha256(m2)
-        if(s1 !=s2):
+    g = secp256k1.G
+    n = secp256k1.q
+    r1 = sig1[0]
+    r2 = sig2[0]
+    s1 = sig1[1]
+    s2 = sig2[1]
+    z1 = sha256(m1)
+    z2 = sha256(m2)
+    
+    if(s1 !=s2):
     # recover, k = 'nonce' & d private key
-            k =(pow((s1-s2),1,n)* pow((z1−z2),−1,n)) % n
-            pre_d = (s1 % n)* pow((k-z1),1,n)*pow(r1,-1,n)
-            d = pre_d % n
+        pre_k =pow((s1-s2),1,n)* pow((z1−z2),−1,n)
+        k = pre_k % n
+        pre_d = pow((s1*k-z1),1,n)*pow(r1,-1,n)
+        d = pre_d % n
 
     # if correct k, then r1 = r2 = kG.x   
-            if ((k*g.x - r1)% n ==0 or (k*g.x + r1)%n ==0 ):
-            
+            if ((k*g.x == r1) or (k*g.x == -r1)):           
     # if correct d, then pk =dG
-                if ((d*g-pk)%n ==0):
+                if (d*g==pk):
                     return d
            
 
