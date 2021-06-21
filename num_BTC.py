cc7abed224eedf87bd8f2c9dc3ea02ee1d379e4e
biodsjung <biodsjung@gmail.com>
2021-06-21 11:59:25 +0900

    update num_BTC

--- num_BTC.py
+++ num_BTC.py
@@ -14,17 +14,19 @@ def num_BTC(b):
     # get quotient and remainder
     q = num_block // 210000
     mod = num_block%210000
-    #print("num_block: {}, mod : {}, quoient : {}".format(num_block, mod, q))
+   
+    print("num_block: {}, mod : {}, quoient : {}".format(num_block, mod, q))
     
     c =0
     if q == 0:
         c = num_block * 50 
     
     else: 
-        c = mod * 50 * pow(2,-q) 
-        #print (pow(2,-q))
+        c = (50 * pow(2,-q)) * mod 
+        print (c)
+        
         for i in range (q+1):
-            pre_c = 50*pow(2,-i)*210000.00
+            pre_c = 50*pow(2,-i)* 210000.00
             c += pre_c
     c = float(c)
     return c
