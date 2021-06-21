3ae3759d0f2559f0617d7f9ca30a392b4e883fee
biodsjung <biodsjung@gmail.com>
2021-06-21 11:15:27 +0900

    update num_BTC

--- num_BTC.py
+++ num_BTC.py
@@ -1,7 +1,28 @@
 import math
 
-def num_BTC(b):
-    c = float(0)
-    return c
+# block 1 ; 50 BTC
+# block 2 ; 100 BTC
+# input is block height(integer)
+# output total number of tokens mined so far
 
+def num_BTC(b):
+    # get total block number
+    num_block = pow(2,b-1)+1
+    
+    # get quotient and remainder
+    q = num_block // 210000
+    mod = num_block % 210000
+    c=0.0
+    
+    if q == 0:
+        c = num_block * 50 
+    
+    else: 
+        for i in range (q):
+            pre_c = 50/2^(i)*210000
+            c += pre_c
+        c += (50/2^q) * mod  
+    
+    c = float(c)
+    return c
 
