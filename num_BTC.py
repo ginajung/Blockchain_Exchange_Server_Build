e7fec218e744c84a6807117a94cc9eeeaab4b059
biodsjung <biodsjung@gmail.com>
2021-06-21 17:07:37 +0900

    update num_BTC

--- num_BTC.py
+++ num_BTC.py
@@ -7,9 +7,9 @@ import math
 
 def num_BTC(b):
     # get total block number
-    num_block=0
-    for h in range (1,b+1):
-        num_block += pow(2,h-1)
+    num_block=b
+#     for h in range (1,b+1):
+#         num_block += pow(2,h-1)
     
     # get quotient and remainder
     q = num_block // 210000
@@ -26,7 +26,7 @@ def num_BTC(b):
         print (c)
         
         for i in range (q+1):
-            pre_c = 50*pow(2,-i)* 210000.00
+            pre_c = 50*pow(2,-i)* 210000
             c += pre_c
     c = float(c)
     return c
