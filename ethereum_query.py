839e7bdfdb5bcb447aacacfd17ffb90585ac3786
biodsjung <biodsjung@gmail.com>
2021-06-25 21:27:15 +0900

    ethereum update

--- ethereum_query.py
+++ ethereum_query.py
@@ -6,9 +6,24 @@ PORT='8545'
 
 w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
 
+
+# >>> web3.eth.get_transaction('0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060')
+# AttributeDict({
+#     'blockHash': '0x4e3a3754410177e6937ef1f84bba68ea139e8d1a2258c5f85db9f1cd715a1bdd',
+#     'blockNumber': 46147,
+#     'from': '0xA1E4380A3B1f749673E270229993eE55F35663b4',
+#     'gas': 21000,
+#     'gasPrice': 50000000000000,
+#     'hash': '0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060',
+#     'input': '0x',
+#     'nonce': 0,
+#     'to': '0x5DF9B87991262F6BA471F09758CDE1c0FC1De734',
+#     'transactionIndex': 0,
+#     'value': 31337,
+# })
 if w3.isConnected():
 #     This line will mess with our autograders, but might be useful when debugging
-    print( "Connected to Ethereum node" )
+#    print( "Connected to Ethereum node" )
 else:
     print( "Failed to connect to Ethereum node!" )
 
@@ -26,6 +41,7 @@ def get_gas_price(tx):
 
 def get_gas(tx):
     gas = 1 #YOUR CODE HERE
+    gas = w3.eth.get_transaction(tx)['gas']
     return gas
 
 def get_transaction_cost(tx):
