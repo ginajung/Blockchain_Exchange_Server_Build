ed302cdb8ceb0ee1a04c7a3bf91e9dae908f9955
biodsjung <biodsjung@gmail.com>
2021-06-28 18:10:56 +0900

    ethereum update

--- ethereum_query.py
+++ ethereum_query.py
@@ -46,6 +46,7 @@ def get_gas(tx):
 
 def get_transaction_cost(tx):
     tx_cost = 1 #YOUR CODE HERE
+    tx_cost = get_gas_price(tx) * get_gas(tx)
     return tx_cost
 
 def get_block_cost(block_num):
