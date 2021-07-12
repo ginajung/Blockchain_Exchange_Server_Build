f3d5439393fee183d9cbbedff59aca0c58ef5fbe
biodsjung <biodsjung@gmail.com>
2021-07-12 20:53:41 +0900

    send token_ver1

--- send_tokens.py
+++ send_tokens.py
@@ -16,21 +16,6 @@ headers = {
 acl = algod.AlgodClient(algod_token, algod_address, headers)  # create the client account
 min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
 
-# Function from Algorand Inc.
-def wait_for_confirmation(client, txid):
-    """
-    Utility function to wait until the transaction is
-    confirmed before proceeding.
-    """
-    last_round = client.status().get('last-round')
-    txinfo = client.pending_transaction_info(txid)
-    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
-        print("Waiting for confirmation")
-        last_round += 1
-        client.status_after_block(last_round)
-        txinfo = client.pending_transaction_info(txid)
-    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
-    return txinfo
 
 
 def send_tokens( receiver_pk, tx_amount ):
@@ -53,24 +38,35 @@ def send_tokens( receiver_pk, tx_amount ):
     # Generate an account
     account_private_key, account_public_key = account.generate_account()
     
-    # create transaction
-    tx = transaction.PaymentTxn(account_public_key, fee, first_valid_round, last_valid_round, gh, receiver_pk, tx_amount)
-    signed_tx = tx.sign(account_private_key)
+    if tx_amount < min_balance:
+        # create transaction
+        tx = transaction.PaymentTxn(account_public_key, fee, first_valid_round, last_valid_round, gh, receiver_pk, tx_amount)
+        signed_tx = tx.sign(account_private_key)
     
     # submit the transaction to the Algorand Testnet
-  
-    acl.send_transaction(signed_tx)
+
+    try:
+        tx_confirm = acl.send_transaction(signed_tx)
+#        print('Transaction sent with ID', txid = signed_tx.transaction.get_txid())
+        wait_for_confirmation(acl, txid = signed_tx.transaction.get_txid())
+    except Exception as e:
+        print(e)
     txid = signed_tx.transaction.get_txid()
-#         print('Transaction sent with ID', txid)
-    wait_for_confirmation(acl, txid)
-#     try:
-#         tx_confirm = acl.send_transaction(signed_tx)
-#         txid = signed_tx.transaction.get_txid()
-#         print('Transaction sent with ID', txid)
-#         wait_for_confirmation(acl, txid)
-#     except Exception as e:
-#         print(e)
-    
     return account_public_key, txid
 
 
+# Function from Algorand Inc.
+def wait_for_confirmation(client, txid):
+    """
+    Utility function to wait until the transaction is
+    confirmed before proceeding.
+    """
+    last_round = client.status().get('last-round')
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
+        print("Waiting for confirmation")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
+    return txinfo
