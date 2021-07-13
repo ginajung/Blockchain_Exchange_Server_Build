b09e032a0d1edf680b728fa336c2a9731efd2145
biodsjung <biodsjung@gmail.com>
2021-07-13 10:01:52 +0900

    verification_signature

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,9 +11,40 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-
+    sig = content['sig']
+    payload = content['payload']
+    message = payload['message']
+    pk=payload['pk']
+    platform=paylod['platform']
+    
+    # to save entire 'payload' dictionary
+    
+#     if content:
+#         content_json = json.dumps(content)
+#     else:
+#         content_json = "no json"   
+    
     #Check if signature is valid
-    result = True #Should only be true if signature validates
+    # for eth and algo 
+    
+    if platform =='Ethereum':
+        
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
+        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,sig)
+        print( eth_sig_obj.messageHash )
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == pk:
+            result = True
+            print( "Eth sig verifies!" )
+    elif platform =='Algorand':
+        
+       
+        algo_sig_str = algosdk.util.sign_bytes(message.encode('utf-8'),sig)
+        if algosdk.util.verify_bytes(message.encode('utf-8'),algo_sig_str,pk):
+            result = True
+            print( "Algo sig verifies!" )
+    else:
+        result = False
+
     return jsonify(result)
 
 if __name__ == '__main__':
