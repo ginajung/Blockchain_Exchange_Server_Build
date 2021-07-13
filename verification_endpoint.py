7043e2b4b53db980fda3fe44ece6826fc47cda99
biodsjung <biodsjung@gmail.com>
2021-07-13 19:22:17 +0900

    verification v1

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -14,31 +14,32 @@ def verify():
 
 #     data1 = json.dumps(content)
 #     data = json.loads(data1)
-
     sig = data['sig']
     payload = data['payload']
     mg = data["payload"][0]["message"]
     pk = data["payload"][0]["pk"]
     platform = data["payload"][0]["platform"]
     
-    # Do we need this step to get string 'payload'?
-    json.dumps(payload)
+ #   json.dumps(payload)
+#     json.dumps(sig)
+#     json.dumps(pk)
+#     json.dumps(platform)
 
     # for eth and algo 
     
     if platform =='Ethereum':
         
         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,sig)
+        eth_sig_obj = sig
         #print( eth_sig_obj.messageHash )
-        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == pk:
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=sig.hex()) == pk:
             result = True
             #print( "Eth sig verifies!" )
             
     elif platform =='Algorand':
 
-        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),sig)
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,pk):
+        #algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),sig)
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
             result = True
             #print( "Algo sig verifies!" )
     else:
