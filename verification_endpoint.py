6dd54d95d1fdabc06aebd5508059d9f91d97325d
biodsjung <biodsjung@gmail.com>
2021-07-14 00:23:35 +0900

    verification v2

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,32 +11,29 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     data = request.get_json()
-#     print( data )
-# #     data1 = json.dumps(content)
-# #     data = json.loads(data1)
-#     sig = data['sig']
-#     pk = data['payload'][0]['pk']
-#     platform = data['payload'][0]['platform']
-#     payload = json.dumps(data['payload'])
+
+    sig = data['sig']
+    pk = data['payload'][0]['pk']
+    platform = data['payload'][0]['platform']
+    payload = json.dumps(data['payload'])
 
     
-#     # for eth and algo 
-#     if platform == "Ethereum":
-        
-#         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-#         eth_sig_obj = sig        
-#         if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
-#             result = True
-#             print( "Eth sig verifies!" )
+    # for eth and algo 
+    if platform == "Ethereum":        
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        eth_sig_obj = sig        
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
+            result = True
+            print( "Eth sig verifies!" )
             
-#     if platform == "Algorand":        
-#         if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
-#             result = True
-#             print( "Algo sig verifies!" )
-# #     else:
-# #         result = False
+    if platform == "Algorand":        
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
+            result = True
+            print( "Algo sig verifies!" )
+#     else:
+#         result = False
 
-    return jsonify(True)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port='5002')
