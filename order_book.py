81b75cc0350d79805f9ea1c78009c33174e57267
biodsjung <biodsjung@gmail.com>
2021-07-13 20:31:57 +0900

    verification v1

---
+++ order_book.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+from models import Base, Order
+engine = create_engine('sqlite:///orders.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def process_order(order):
+    #Your code here
+    pass
