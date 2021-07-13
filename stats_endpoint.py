159a1c0825e775f44e2181a96d36b61a666323b1
biodsjung <biodsjung@gmail.com>
2021-07-13 10:22:28 +0900

    verification_signature

--- stats_endpoint.py
+++ stats_endpoint.py
@@ -4,20 +4,30 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
+numbers =[]
 
-cumulative = ['x']
 @app.route('/store', methods=['GET','POST'])
 def store():
     content = request.get_json(silent=True)
     #Your code here
-    cumulative.append(content)
-    return jsonify(cumulative )
+    numbers =[]
+    result = numbers.append(content['x'])
+    return jsonify( result )
 
 @app.route('/retrieve', methods=['GET','POST'])
 def retrieve():
     content = request.get_json(silent=True)
     #Your code here
-    result = cumulative.content
+    #JSON object with a single field "function". {'function': 'mean'}
+    if content['function']== 'mean':
+        result = mean(numbers)
+    elif content['function']== 'min':
+        result = min(numbers)
+    elif content['function']== 'max':
+        result = max(numbers)
+    else: 
+        print('error')
+        
     return jsonify( result )
 
 if __name__ == '__main__':
