e07430630df561c09fa32a59cbfbb6aa83452113
biodsjung <biodsjung@gmail.com>
2021-07-13 19:32:44 +0900

    verification v1

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,35 +10,32 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    data = request.get_json(force=True)
+    data = request.get_json(silent=True)
 
 #     data1 = json.dumps(content)
 #     data = json.loads(data1)
     sig = data['sig']
     payload = data['payload']
-    mg = data["payload"][0]["message"]
     pk = data["payload"][0]["pk"]
     platform = data["payload"][0]["platform"]
     
- #   json.dumps(payload)
-#     json.dumps(sig)
-#     json.dumps(pk)
-#     json.dumps(platform)
+    json.dumps(payload)
+    json.dumps(sig)
+    json.dumps(pk)
+    json.dumps(platform)
 
-    # for eth and algo 
     
+    # for eth and algo 
     if platform =='Ethereum':
         
         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = sig
-        #print( eth_sig_obj.messageHash )
+        eth_sig_obj = sig        
         if eth_account.Account.recover_message(eth_encoded_msg,signature=sig.hex()) == pk:
             result = True
             #print( "Eth sig verifies!" )
             
     elif platform =='Algorand':
-
-        #algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),sig)
+        
         if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
             result = True
             #print( "Algo sig verifies!" )
