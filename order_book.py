940882e7720f24a2967317741bebf3f3b6655366
biodsjung <biodsjung@gmail.com>
2021-07-15 20:37:26 +0900

    project2

--- order_book.py
+++ order_book.py
@@ -18,26 +18,31 @@ def process_order(new_order):
     session.commit()
     
 # CHECK MATCH : check if matching to any existing orders
-    #orders = session.query(Order).all()
-    for existing_order in session:
+    orders = session.query(Order).filter(Order.filled == None).all()
+    orders = orders.filter(Order.buy_currency == new_order_obj.sell_currency)
+    orders = orders.filter(Order.buy_currency==new_order_obj.buy_currency)
+    orders = orders.filter(Order.sell_amount / Order.buy_amount >= new_order_obj.buy_amount/new_order_obj.sell_amount)
+    
+    for existing_order in orders:
         
-        if existing_order.buy_currency == new_order_obj.sell_currency and \
-        existing_order.sell_currency == new_order_obj.buy_currency and \
-        existing_order.sell_amount / existing_order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount:
+        
+#         if existing_order.buy_currency == new_order_obj.sell_currency and \
+#         existing_order.sell_currency == new_order_obj.buy_currency and \
+#         existing_order.sell_amount / existing_order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount:
             
-            # Handle matching order
+#         # Handle matching order
             
             # Set the filled field to be the current timestamp on both orders
-            new_order_obj.filled = datetime.now()
-            existing_order.filled = datetime.now()  
+        new_order_obj.filled = datetime.now()
+        existing_order.filled = datetime.now()  
             
             # Set counterparty_id to be the id of the other order
-            new_order_obj.counterparty_id = existing_order.id  
-            existing_order.counterparty_id = new_order_obj.id 
+        new_order_obj.counterparty_id = existing_order.id  
+        existing_order.counterparty_id = new_order_obj.id 
             
-            # 3. If one of the orders is not completely filled 
-            #.   (i.e. the counterparty’s sell_amount is less than buy_amount):
-            if existing_order.sell_amount<= new_order_obj.buy_amount:
+        # 3. If one of the orders is not completely filled 
+        #.   (i.e. the counterparty’s sell_amount is less than buy_amount):
+        if new_order_obj.sell_amount >= existing_order.buy_amount:
                 # 4 Create a new order for remaining balance ==> make_order? 
                 #       - The new order should have the created_by field set to the id of its parent order
                 #       - The new order should have the same pk and platform as its parent order
@@ -55,19 +60,42 @@ def process_order(new_order):
                 child_order_new['sell_amount'] = random.randint(exchange_rate,10)
     
     
-                child_order_obj = Order( sender_pk=child_order['sender_pk'],receiver_pk=child_order['receiver_pk'], \
-                                        buy_currency=child_order['buy_currency'],sell_currency=child_order['sell_currency'],\
-                                        buy_amount=child_order['buy_amount'], sell_amount=child_order['sell_amount'] )
+                child_order_newobj = Order( sender_pk=child_order_new['sender_pk'],receiver_pk=child_order_new['receiver_pk'], \
+                                        buy_currency=child_order_new['buy_currency'],sell_currency=child_order_new['sell_currency'],\
+                                        buy_amount=child_order_new['buy_amount'], sell_amount=child_order_new['sell_amount'] )
 
-#               session.add(child_order_obj) 
-                            
-                child_order_obj.filled = datetime.now() 
-                child_order_obj.creator_id = new_order_obj.id
+                session.add(child_order_newobj) 
+                
+        
+                child_order_newobj.filled = datetime.now() 
+                child_order_newobj.creator_id = new_order_obj.id
+                session.commit()
+                
+        if new_order_obj.buy_amount <= existing_order.sell_amount:
+                child_order_ex = {}
+                child_order_ex['sender_pk'] = existing_order.sender_pk
+                child_order_ex['receiver_pk'] = existing_order.receiver_pk
+                child_order_ex['buy_currency'] = existing_order.buy_currency
+                child_order_ex['sell_currency'] = existing_order.sell_currency
+                child_order_ex['buy_amount'] = existing_order.buy_amount
+    
+                #any value such that the implied exchange rate of the new order is at least that of the old order
+                
+                exchange_rate_ex = existing_order.buy_amount/existing_order.sell_amount
+                child_order_ex['sell_amount'] = random.randint(exchange_rate,10)
+                
+                child_order_exobj = Order( sender_pk=child_order_ex['sender_pk'],receiver_pk=child_order_ex['receiver_pk'], \
+                                        buy_currency=child_order_ex['buy_currency'],sell_currency=child_order_ex['sell_currency'],\
+                                        buy_amount=child_order_ex['buy_amount'], sell_amount=child_order_ex['sell_amount'] )
+
+                session.add(child_order_exobj) 
+                child_order_exobj.filled = datetime.now() 
+                child_order_exobj.creator_id = existing_order.id
+                
                 #new_order_obj.child = child_order_obj
                 session.commit()
                 
-             #if new_order_obj.sell_amount<= existing_order.buy_amount:    
-                
+               
                 pass
     
 
