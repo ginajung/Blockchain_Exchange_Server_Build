971ff4ed5aa5ae36f4eeb86ab0c68da898059c50
biodsjung <biodsjung@gmail.com>
2021-07-15 20:46:44 +0900

    project2

--- order_book.py
+++ order_book.py
@@ -65,7 +65,7 @@ def process_order(new_order):
                                         buy_currency=child_order_new['buy_currency'],sell_currency=child_order_new['sell_currency'],\
                                         buy_amount=child_order_new['buy_amount'], sell_amount=child_order_new['sell_amount'] )
 
-                session.add(child_order_newobj) 
+                #session.add(child_order_newobj) 
                 
         
                 child_order_newobj.filled = datetime.now() 
@@ -90,7 +90,7 @@ def process_order(new_order):
                                         buy_currency=child_order_ex['buy_currency'],sell_currency=child_order_ex['sell_currency'],\
                                         buy_amount=child_order_ex['buy_amount'], sell_amount=child_order_ex['sell_amount'] )
 
-                session.add(child_order_exobj) 
+                #session.add(child_order_exobj) 
                 child_order_exobj.filled = datetime.now() 
                 child_order_exobj.creator_id = existing_order.id
                 
