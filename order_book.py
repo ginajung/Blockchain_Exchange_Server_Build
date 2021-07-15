9631c2461f2f182b072f5dcb814abb0d7b4d39f7
biodsjung <biodsjung@gmail.com>
2021-07-15 20:41:26 +0900

    project2

--- order_book.py
+++ order_book.py
@@ -19,30 +19,31 @@ def process_order(new_order):
     
 # CHECK MATCH : check if matching to any existing orders
     orders = session.query(Order).filter(Order.filled == None).all()
-    orders = orders.filter(Order.buy_currency == new_order_obj.sell_currency)
-    orders = orders.filter(Order.buy_currency==new_order_obj.buy_currency)
-    orders = orders.filter(Order.sell_amount / Order.buy_amount >= new_order_obj.buy_amount/new_order_obj.sell_amount)
+#     orders = orders.filter(Order.buy_currency == new_order_obj.sell_currency)
+#     orders = orders.filter(Order.buy_currency==new_order_obj.buy_currency)
+#     orders = orders.filter(Order.sell_amount / Order.buy_amount >= new_order_obj.buy_amount/new_order_obj.sell_amount)
     
     for existing_order in orders:
         
         
-#         if existing_order.buy_currency == new_order_obj.sell_currency and \
-#         existing_order.sell_currency == new_order_obj.buy_currency and \
-#         existing_order.sell_amount / existing_order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount:
+        if existing_order.buy_currency == new_order_obj.sell_currency and \
+        existing_order.sell_currency == new_order_obj.buy_currency and \
+        existing_order.sell_amount / existing_order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount:
             
 #         # Handle matching order
             
             # Set the filled field to be the current timestamp on both orders
-        new_order_obj.filled = datetime.now()
-        existing_order.filled = datetime.now()  
+            new_order_obj.filled = datetime.now()
+            existing_order.filled = datetime.now()  
             
             # Set counterparty_id to be the id of the other order
-        new_order_obj.counterparty_id = existing_order.id  
-        existing_order.counterparty_id = new_order_obj.id 
+            new_order_obj.counterparty_id = existing_order.id  
+            existing_order.counterparty_id = new_order_obj.id 
             
         # 3. If one of the orders is not completely filled 
         #.   (i.e. the counterparty’s sell_amount is less than buy_amount):
-        if new_order_obj.sell_amount >= existing_order.buy_amount:
+            if new_order_obj.sell_amount >= existing_order.buy_amount:
+                
                 # 4 Create a new order for remaining balance ==> make_order? 
                 #       - The new order should have the created_by field set to the id of its parent order
                 #       - The new order should have the same pk and platform as its parent order
@@ -71,7 +72,8 @@ def process_order(new_order):
                 child_order_newobj.creator_id = new_order_obj.id
                 session.commit()
                 
-        if new_order_obj.buy_amount <= existing_order.sell_amount:
+            if new_order_obj.buy_amount <= existing_order.sell_amount:
+                
                 child_order_ex = {}
                 child_order_ex['sender_pk'] = existing_order.sender_pk
                 child_order_ex['receiver_pk'] = existing_order.receiver_pk
