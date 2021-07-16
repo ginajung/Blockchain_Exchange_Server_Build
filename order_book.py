c7bec76a00f048bbec648ea84b491be6a27de465
biodsjung <biodsjung@gmail.com>
2021-07-16 09:23:48 +0900

    project2

--- order_book.py
+++ order_book.py
@@ -17,7 +17,7 @@ def process_order(new_order):
     session.add(new_order_obj)
     session.commit()
     
-# CHECK MATCH : check if matching to any existing orders
+# CHECK MATCH : check if matching to any existing order, stop
     orders = session.query(Order).filter(Order.filled == None).all()   
    
     for existing_order in orders:    
@@ -25,8 +25,8 @@ def process_order(new_order):
         if existing_order.buy_currency == new_order_obj.sell_currency and \
         existing_order.sell_currency == new_order_obj.buy_currency and \
         existing_order.sell_amount / existing_order.buy_amount >= new_order_obj.buy_amount/new_order_obj.sell_amount:
-            
-        # Handle matching order            
+    
+     # Handle matching order            
             # Set the filled field to be the current timestamp on both orders
             new_order_obj.filled = datetime.now()
             existing_order.filled = datetime.now()  
@@ -34,26 +34,24 @@ def process_order(new_order):
             # Set counterparty_id to be the id of the other order
             new_order_obj.counterparty_id = existing_order.id  
             existing_order.counterparty_id = new_order_obj.id 
+            
             break;
-        # 3. If one of the orders is not completely filled 
-        #.   (i.e. the counterparty’s sell_amount is less than buy_amount):
+
+    # 3. If one of the orders is not completely filled (i.e. the counterparty’s sell_amount is less than buy_amount):
     if new_order_obj.sell_amount > existing_order.buy_amount :
         
                 
-                # 4 Create a new order for remaining balance ==> make_order? 
-                #       - The new order should have the created_by field set to the id of its parent order
-                #       - The new order should have the same pk and platform as its parent order
-                #       - The sell_amount of the new order can be any value such that 
-                #.        the implied exchange rate of the new order is at least that of the old order
+    # 4 Create a new order for remaining balance 
+        #  created_by field set to the id of its parent order
+        #  the same pk and platform as its parent order
+        #  The sell_amount of the new order can be any value such that the implied exchange rate of the new order is at least that of the old order
         child_order_new = {}
         
         child_order_new['sender_pk'] = new_order_obj.sender_pk
         child_order_new['receiver_pk'] = new_order_obj.receiver_pk
         child_order_new['buy_currency'] = new_order_obj.buy_currency
         child_order_new['sell_currency'] = new_order_obj.sell_currency
-                
-    
-                #any value such that the implied exchange rate of the new order is at least that of the old order
+ 
         exchange_rate_new = new_order_obj.buy_amount/new_order_obj.sell_amount
             
         child_sell_amount = new_order_obj.sell_amount-existing_order.buy_amount
@@ -67,13 +65,11 @@ def process_order(new_order):
                                         buy_amount=child_order_new['buy_amount'], sell_amount=child_order_new['sell_amount'] )
 
         session.add(child_order_newobj) 
-                #child_order_newobj.filled = datetime.now() 
         child_order_newobj.creator_id = new_order_obj.id
-                #new_order_obj.child = child_order_newobj
         session.commit()
                 
     if new_order_obj.buy_amount < existing_order.sell_amount :
-                
+        # child order for counterparty
         child_order_ex = {}
         child_order_ex['sender_pk'] = existing_order.sender_pk
         child_order_ex['receiver_pk'] = existing_order.receiver_pk
@@ -96,23 +92,9 @@ def process_order(new_order):
                                         buy_amount=child_order_ex['buy_amount'], sell_amount=child_order_ex['sell_amount'] )
 
         session.add(child_order_exobj) 
-                #child_order_exobj.filled = datetime.now() 
         child_order_exobj.creator_id = existing_order.id
-                #existing_order.child = child_order_exobj
-               
         session.commit()
                 
                
         pass
-    
-
-
-# Fill the child-order
-# 1. Each order matches at most one other (to match one order against multiple others create derivative orders, 
-#    and set the “created_by” field as described above)
-# 2. Any derived orders must have an implied exchange rate that is at least the original exchange rate, 
-#.   i.e., buy_amount/sell_amount on the new order must be at least the buy_amount/sell_amount on the order that created it
-
-
-
     
