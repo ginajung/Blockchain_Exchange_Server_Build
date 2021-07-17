9e36027d098ad787acb056d0427ddea1cbde2f3c
biodsjung <biodsjung@gmail.com>
2021-07-17 17:53:09 +0900

    project2

--- ZK_equality.py
+++ ZK_equality.py
@@ -12,17 +12,20 @@ def ZK_equality(G,H):
 #     r1=Secret()
 #     r2=Secret()
     m = Secret()
-
+    
+    
     C1 = r1*G
     C2 = r1*H + m*G
     D1 = r2*G
     D2 = r2*H + m*G
     
+    Enc1=(C1,C2)
+    Enc2=(D1,D2)
     #Generate a NIZK proving equality of the plaintexts
     
     stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
     zk_proof = stmt.prove()
     
     #Return two ciphertexts and the proof
-    return (C1,C2), (D1,D2), zk_proof
+    return Enc1, Enc2, zk_proof
 
