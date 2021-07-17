0a32cb759971e024b797b8a1613d3e6849d67ee7
biodsjung <biodsjung@gmail.com>
2021-07-17 17:39:37 +0900

    project2

--- ZK_equality.py
+++ ZK_equality.py
@@ -5,21 +5,18 @@ def ZK_equality(G,H):
 
   
    #Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
- 
-
-
     
     # Setup: generate a secret randomizer for the commitment scheme.
-    r1 = Secret(utils.get_random_num(bits=128))
-
-    r2 = Secret(utils.get_random_num(bits=128))
-
+#     r1 = Secret(utils.get_random_num(bits=128))
+#     r2 = Secret(utils.get_random_num(bits=128))
+    r1=Secret()
+    r2=Secret()
     m = Secret()
 
     C1 = r1*G
-    C2 = r1*H+m*G
+    C2 = r1*H + m*G
     D1 = r2*G
-    D2 = r2*H+m*G
+    D2 = r2*H + m*G
     
     #Generate a NIZK proving equality of the plaintexts
     
