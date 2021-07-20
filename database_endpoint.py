08c29f0a7a7fdf14044dc7571300397b82723b2e
biodsjung <biodsjung@gmail.com>
2021-07-20 10:38:49 +0900

    project3

--- database_endpoint.py
+++ database_endpoint.py
@@ -32,7 +32,7 @@ def shutdown_session(response_or_exc):
 -------- Helper methods (feel free to add your own!) -------
 """
 
-def log_message(d)
+def log_message(d):
     # Takes input dictionary d and writes it to the Log table
     
     
