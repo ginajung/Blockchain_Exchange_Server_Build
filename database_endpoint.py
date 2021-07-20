423198e8bd343723d143fc4ce1606f639505afe9
biodsjung <biodsjung@gmail.com>
2021-07-20 16:47:17 +0900

    database_endpoint

--- database_endpoint.py
+++ database_endpoint.py
@@ -93,7 +93,7 @@ def trade():
 
         # if verified, insert into Order table
         if result == True :
-            new_order_obj = Order(sender_pk=content['payload']['sender_pk'],receiver_pk=content['payload']['receiver_pk'],buy_currency=content['payload']['buy_currency'], sell_currency=content['payload']['sell_currency'],buy_amount=content['payload']['buy_amount'], sell_amount=content['payload']['sell_amount'])
+            new_order_obj = Order(sender_pk=content['payload']['sender_pk'],receiver_pk=content['payload']['receiver_pk'],buy_currency=content['payload']['buy_currency'], sell_currency=content['payload']['sell_currency'],buy_amount=content['payload']['buy_amount'], sell_amount=content['payload']['sell_amount'], signature = payload)
    
             g.session.add(new_order_obj)
             g.session.commit()
