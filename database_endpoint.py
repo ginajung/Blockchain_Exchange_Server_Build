350ee0b3020b107e36bd00ae79fca30e27d68812
biodsjung <biodsjung@gmail.com>
2021-07-21 14:29:36 +0900

    project3

--- database_endpoint.py
+++ database_endpoint.py
@@ -106,14 +106,14 @@ def trade():
             g.session.commit()
         
         
-        return(True)
+       # return(True)
             
 @app.route('/order_book')
 def order_book():
     #Your code here : return a list of all orders in the database.
     #Note that you can access the database session using g.session   
-#     orders = g.session.query(Order).filter(Order.sender_pk !=None, Order.receiver_pk !=None, Order.buy_currency !=None, Order.sell_currency !=None, Order.buy_amount!=None, Order.sell_amount!=None, Order.signature!=None).all() 
-    orders = g.session.query(Order).all()
+    orders = g.session.query(Order).filter(Order.sender_pk !=None, Order.receiver_pk !=None, Order.buy_currency !=None, Order.sell_currency !=None, Order.buy_amount!=None, Order.sell_amount!=None, Order.signature!=None).all() 
+#     orders = g.session.query(Order).all()
     data =[]
     
 #     # save orders as a list of dicts / convert to JSON
