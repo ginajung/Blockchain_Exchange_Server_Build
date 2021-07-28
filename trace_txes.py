484e55cb03bd265962f41765f0b0c443021a6030
biodsjung <biodsjung@gmail.com>
2021-07-28 15:22:29 +0900

    tracing BTC

--- trace_txes.py
+++ trace_txes.py
@@ -81,9 +81,7 @@ class TXO:
 
 
     def get_inputs(self,d=1):
-        
             
-             
         # - connect to the Bitcoin blockchain, 
         # try to find parent of self... could be many (self.inputs)
         
@@ -95,11 +93,11 @@ class TXO:
             
             parent_tx = self_tx['vin']
         
-                for tx in parent_tx:
-                    tx_id = tx['txid'] 
-                    get_input_tx = rpc_connection.getrawtransaction(tx_id,True)
-                    tx_oj = TXO.from_tx_hash(tx_id, n=tx['vout'])
-                    self.inputs.append(tx_oj)
+            for tx in parent_tx:
+                tx_id = tx['txid'] 
+                get_input_tx = rpc_connection.getrawtransaction(tx_id,True)
+                tx_oj = TXO.from_tx_hash(tx_id, n=tx['vout'])
+                self.inputs.append(tx_oj)
                     
         if d=2:
             TXO.get_inputs(tx in self.inputs)
