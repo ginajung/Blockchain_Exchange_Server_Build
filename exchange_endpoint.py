2e0499d5a17462e76a7293216b4ba2d39db4a158
biodsjung <biodsjung@gmail.com>
2021-08-13 22:20:11 +0900

    project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -119,12 +119,12 @@ def get_algo_keys():
 def get_eth_keys(filename = "eth_mnemonic.txt"):
     w3 = Web3()
     
-    # w3.eth.account.enable_unaudited_hdwallet_features()
-    # acct,mnemonic_secret = w3.eth.account.create_with_mnemonic()
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    acct,mnemonic_secret = w3.eth.account.create_with_mnemonic()
     # TODO: Generate or read (using the mnemonic secret) 
     # the ethereum public/private keys
-    mnemonic_secret = "sight garment riot tattoo tortoise  talk sea ill walnut leg robot myth toe perfect rifle dizzy spend april build legend brother above hospital"
-    acct = w3.eth.account.from_mnemonic(mnemonic_secret)
+    # mnemonic_secret = "sight garment riot tattoo tortoise  talk sea ill walnut leg robot myth toe perfect rifle dizzy spend april build legend brother above hospital"
+    # acct = w3.eth.account.from_mnemonic(mnemonic_secret)
     eth_pk = acct._address
     eth_sk = acct._private_key
     
