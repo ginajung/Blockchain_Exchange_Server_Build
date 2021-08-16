4eb12cf4254dbefa73b06f3eb3a6d067dc64a3ed
biodsjung <biodsjung@gmail.com>
2021-08-16 21:06:09 +0900

    updated project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -114,7 +114,7 @@ def get_algo_keys():
 
     # TODO: Generate or read (using the mnemonic secret)
     # the algorand public/private keys
-    mnemonic_secret = "exist moral soap aspect job word upgrade shift misery raise limit describe sport club toddler liar pig danger face blossom stand conduct mistake elephant arrive"
+    mnemonic_secret = "exotic tuna make peace raw dizzy music heart cycle oval juice cinnamon argue memory inner minor decorate custom market emotion tower sign exact drop solar"
   
     algo_sk = mnemonic.to_private_key(mnemonic_secret)
     algo_pk = mnemonic.to_public_key(mnemonic_secret)
