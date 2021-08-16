d614bfcc654132650d8b233f281b83249a150cbe
biodsjung <biodsjung@gmail.com>
2021-08-16 21:02:12 +0900

    updated project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -114,7 +114,7 @@ def get_algo_keys():
 
     # TODO: Generate or read (using the mnemonic secret)
     # the algorand public/private keys
-    mnemonic_secret = "exist soap aspect job word upgrade shift misery raise limit describe sport club toddler liar pig danger face blossom stand conduct mistake elephant arrive"
+    mnemonic_secret = "exist exist soap aspect job word upgrade shift misery raise limit describe sport club toddler liar pig danger face blossom stand conduct mistake elephant arrive"
   
     algo_sk = mnemonic.to_private_key(mnemonic_secret)
     algo_pk = mnemonic.to_public_key(mnemonic_secret)
