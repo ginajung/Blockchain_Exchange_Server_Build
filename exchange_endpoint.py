0368a68f040828b9df02c03a79dcbc61ec764ade
biodsjung <biodsjung@gmail.com>
2021-08-16 18:19:35 +0900

    updated project V

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -114,11 +114,12 @@ def get_algo_keys():
 
     # TODO: Generate or read (using the mnemonic secret)
     # the algorand public/private keys
-    # mnemonic_secret = "want quiz moral bread repeat embark shed steak book joy fetch pilot shift floor minimum poverty index yard cannon divorce fatal angry mistake abandon voyage"
-    # algo_sk = mnemonic.to_private_key(mnemonic_secret)
-    # algo_pk = mnemonic.to_public_key(mnemonic_secret)
+    mnemonic_secret = "soft acid moral bread repeat action shed actual chalk joy fetch pilot shift floor identify poverty index yard cannon divorce fatal angry mistake abandon voyage"
+  
+    algo_sk = mnemonic.to_private_key(mnemonic_secret)
+    algo_pk = mnemonic.to_public_key(mnemonic_secret)
     
-    algo_sk, algo_pk = account.generate_account()
+    #algo_sk, algo_pk = account.generate_account()
 
     return algo_sk, algo_pk
 
