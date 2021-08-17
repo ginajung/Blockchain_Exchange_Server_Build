1a990cddd2859f564871fcbf4b0d409d9c47c43a
biodsjung <biodsjung@gmail.com>
2021-08-17 12:14:24 +0900

    last up V5

--- send_tokens.py
+++ send_tokens.py
@@ -149,11 +149,12 @@ def send_tokens_eth(w3,sender_sk,txes):
         tx_dict = {
                 'nonce': starting_nonce+i, #Locally update nonce
                 'gasPrice':w3.eth.gas_price,
-                'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': receiver_pk, 'data': b'', 'amount': amt } ),
+                'gas': 1 ,
                 'to': receiver_pk,
                 'value': amt,
                 'data':b'' }
                 
+                #'gas': w3.eth.estimate_gas( { 'from': sender_pk, 'to': receiver_pk, 'data': b'', 'amount': amt }
         signed_txn = w3.eth.account.sign_transaction(tx_dict, sender_sk)
         tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
         wait_for_confirmation_eth(w3, tx_id )
