afa6ff76ce184f9ff804cf507f0b08e0b8c75f40
biodsjung <biodsjung@gmail.com>
2021-08-17 12:54:15 +0900

    last up V5

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -315,7 +315,7 @@ def execute_txes(txes):
 
         eth_txid = send_tokens_eth(w3,eth_sk,eth_tx)
         
-        new_tx_object = TX(platform = "Ethereum", receiver_pk = eth_tx["receiver_pk"], order_id= eth_tx['order_id'], tx_id = eth_txid )
+        new_tx_object = TX(platform = "Algorand", receiver_pk = eth_tx["receiver_pk"], order_id= eth_tx['order_id'], tx_id = eth_txid )
         g.session.add(new_tx_object)
         g.session.commit()
 
@@ -323,7 +323,7 @@ def execute_txes(txes):
 
     for algo_tx in algo_txes:
         algo_txid = send_tokens_algo(acl,algo_sk,algo_tx)
-        new_tx_object = TX(platform = "Algorand", receiver_pk = algo_tx['receiver_pk'], order_id= algo_tx['order_id'], tx_id = algo_txid )
+        new_tx_object = TX(platform = "Ethereum", receiver_pk = algo_tx['receiver_pk'], order_id= algo_tx['order_id'], tx_id = algo_txid )
         g.session.add(new_tx_object)
         g.session.commit()
 
